1bed5118e030c6dbe91aa004f26a7400f476872a
abhaypsxd <theabhay626@gmail.com>
2024-04-11 11:29:46 +0530

    add code to get category wise dataframes

--- analysis.py
+++ analysis.py
@@ -8,9 +8,16 @@ df = pd.read_csv("JPN[1].csv")
 df_mini = df.iloc[0:18, :]
 # print(df_mini)
 
-category_list = df['Type'].dropna().tolist()
+category_list = df['Type'].tolist()
+category_list_values = df['Type'].dropna().tolist()
 category_list_index = []
 for i in range(len(category_list)):
-    if category_list[i] != category_list[1]:
-        category_list_index.append(i)
-print(category_list_index)
+    for j in category_list_values:
+        if category_list[i] == j:
+            category_list_index.append(i)
+category_wise_df = []
+for i in category_list_index:
+    df1 = df.iloc[i:i+1, :]
+    category_wise_df.append(df1)
+for i in category_wise_df:
+    print(i)
