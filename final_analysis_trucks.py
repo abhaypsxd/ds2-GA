68de90f13b823f8a9e07b02b81fee50798316698
abhaypsxd <theabhay626@gmail.com>
2024-04-11 19:35:00 +0530

    add trucks and cars in csv format

--- final_analysis_trucks.py
+++ final_analysis_trucks.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib.gridspec import GridSpec
 
 trucks = ['Toyota','Nissan','Mitsubishi Fuso','Hino','Isuzu','UD Trucks (2008-)','UD Trucks']
+trucks1 = ['Toyota','Nissan','Mitsubishi Fuso','Hino','Isuzu','UD Trucks']
+yearwise_trucks = []
 yearwise_trucks = []
 
 for i in range(2017,2024):
@@ -18,4 +20,14 @@ for i in range(2017,2024):
             if i > a[0] and i < b[0]:
                 l1.append(df_mini.loc[i]['Total'].tolist())
     yearwise_trucks.append(l1)
-print(yearwise_trucks)
+dict_list=[]
+for i in yearwise_trucks:
+    dict1={}
+    for j in range(len(i)):
+        dict1[trucks1[j]]=i[j]
+    dict_list.append(dict1)
+
+df = pd.DataFrame(dict_list, index=range(2017,2024))
+df.index.name='Year'
+df.to_csv('trucks.csv')
+
